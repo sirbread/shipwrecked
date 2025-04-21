1861e65b0504447c4e94acbefcf5e56cdce72f52
Josias <ndjosiasaurel@gmail.com>
2025-04-21 16:46:59 +0100

    resolve conflicts

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -1,8 +1,76 @@
 import { fetchHackatimeProjects } from "@/lib/hackatime";
+import Airtable from "airtable";
+
+Airtable.configure({ 
+   apiKey: process.env.AIRTABLE_PAT
+})
+
+const airtable = new Airtable();
+const base = airtable.base(process.env.AIRTABLE_BASE ?? "");
+const projectsBase = base("Projects");
+
+export type Project = {
+    id: string
+    name: string,
+    description: string
+    codeUrl: string,
+    playableUrl?: string
+    hackatimeProject?: string
+}
+
+const getProjectsByUser = () => new Promise<Project[]>((resolve, reject) => {
+    const projects: Project[] = [];
+    projectsBase.select({ view: "Grid view" }).eachPage((records, fetchNextPage) => {
+        records.forEach(record => {
+            projects.push({
+                id: record.id,
+                name: record.get("Name") as string,
+                description: record.get("Description") as string,
+                codeUrl: record.get("Code URL") as string,
+                playableUrl: record.get("Playable URL") as string
+            } as Project);
+        })
+        fetchNextPage();
+    }, (err) => {
+        if (err) return reject();
+        return resolve(projects);
+    });
+});
 
 export async function GET(request: Request) { 
-    const josiasSlackID = "U01PJ08PR7S";
-    const hackatimeProjects = await fetchHackatimeProjects(josiasSlackID);
+    const { searchParams } = new URL(request.url);
+    // const josiasSlackID = "U01PJ08PR7S";
+    // const hackatimeProjects = await fetchHackatimeProjects(josiasSlackID);
+    if (searchParams.has("hackatime")) 
+        return Response.json(await fetchHackatimeProjects(searchParams.get("slackID") as string))
 
-    return Response.json(hackatimeProjects);
+    const projects = await getProjectsByUser();
+
+    return Response.json(projects);
+}
+
+export const createProjectAirtable = async ({ codeUrl, playableUrl, description, name, hackatime }: any) => new Promise((resolve, reject) => {
+    const payload = {
+        "Code URL": codeUrl,
+        "Playable URL": playableUrl,
+        "Name": name,
+        "Description": description,
+        "Hackatime Project": hackatime
+    };
+    projectsBase.create([{
+        "fields": payload
+    }], (err: any, _: any) => {
+        if (err) return reject(err);
+        return resolve(payload);
+    });
+});
+
+export async function POST(request: Request) {
+    const { name, description, hackatime, codeUrl, playableUrl } = await request.json();
+    try {
+        const createdProject = await createProjectAirtable({ name, description, hackatime, codeUrl, playableUrl});
+        return Response.json({ success: true, data: createdProject });
+    } catch (err) {
+        return Response.json({ success: false, err });
+    }
 }
--- components/common/Project.tsx
+++ components/common/Project.tsx
@@ -1,16 +1,35 @@
 'use client'
+import type { Project } from "@/app/api/projects/route"
+import Icon from "@hackclub/icons"
 
-
-type Project = {
-    name: string
-    timeSpent: string
-}
-
-export function Project({ name, timeSpent } : Project) {
+export function Project({ name, description, codeUrl, playableUrl }: Project) {
     return (
-        <div>
-            {name} - {timeSpent}
-            
+        <div
+            className="p-4 rounded-xl relative w-full max-w-md overflow-hidden border-0 bg-[#38b6e3] shadow-lg"
+        >
+            <div
+                className="absolute bottom-0 left-0 right-0 h-16 bg-[#f9e9c7]"
+                style={{
+                    clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 85% 25%, 70% 0%, 55% 25%, 40% 0%, 25% 25%, 10% 0%, 0% 20%)",
+                }}
+            />
+            <h2 className="flex flex-row items-center font-sans text-3xl font-extrabold uppercase tracking-wider text-white">
+                <Icon glyph="explore" size={60} />
+                {name}
+            </h2>
+            <p className="text-md font-medium text-white mx-6">
+                {description}
+            </p>
+            <div className="relative w-full flex flex-wrap items-center gap-2">
+                <a href={codeUrl} className="flex flex-row items-center gap-2 w-40 h-8 m-2 rounded-lg bg-[#ffd84d] px-3 py-2 text-md font-bold text-[#1a5e7a] transition-transform hover:scale-105">
+                        <Icon glyph="code" size={30} />
+                        View Code
+                </a>
+                <a href={playableUrl} className="flex flex-row items-center gap-2 w-40 h-8 rounded-lg bg-white px-3 py-2 text-md font-bold text-[#1a5e7a] transition-transform hover:scale-105">
+                    <Icon glyph="link" size={30} />
+                    Try It!
+                </a>
+            </div>
         </div>
     )
 }
